7bdd0ddba3da1ada3decb356deca41d6b9b28f44
Steni Koilraj <stenikoil96@gmail.com>
2020-09-22 17:45:32 +0400

    Add bg, heading, icon

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>TO DO</h1>
+      <h2> What would you like to do today? </h2>
+      <input type="text"></input><button>Add</button>
+      {/* list of todos */}
     </div>
   );
 }
