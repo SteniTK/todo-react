f492767f4e65b56709767c18384790b48d4a545b
Steni Koilraj <stenikoil96@gmail.com>
2020-09-24 16:29:39 +0400

    Add button

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <h1>TO DO</h1>
-      <h2> What would you like to do today? </h2>
-      <input type="text"></input><button>Add</button>
+      <h2>What would you like to do today?</h2>
+      <input type="text" className="entry"></input>
+      <button className="submit-button">
+        <img src={process.env.PUBLIC_URL + "edit.png"} alt="add-entry"></img>
+      </button>
       {/* list of todos */}
     </div>
   );
