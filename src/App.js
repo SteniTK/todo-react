f227027aeef040c286f1ba20bd97fea956590ec2
Steni Koilraj <stenikoil96@gmail.com>
2020-10-03 10:45:31 +0400

    add todolist component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import './App.css';
-import TodoItem from './TodoItem/TodoItem.js';
+import TodoList from './TodoList/TodoList.js';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.todoEntries = [
+      {label:"Get garbage", status:"completed"},
+      {label:"Grocery shop", status:"in-progress"}
+    ];
+  }
+  render () {
+    return (<div className="App">
       <h1>TO DO</h1>
       <h2>What would you like to do today?</h2>
       <div className="input">
         <input type="text" className="entry"></input>
-        <input type="image"  className="submit-button" src={process.env.PUBLIC_URL + "edit.png"} alt="Add entry" />
+        <input type="image"  className="submit-button" src={process.env.PUBLIC_URL + "edit.png"} alt="Add entry" onClick={this.submitEntry}/>
       </div>
-      <ul>
-        <TodoItem label="Item#1" status="completed"></TodoItem>
-        <TodoItem label="Item#2" status="in-progress"></TodoItem>
-      </ul>
-    </div>
-  );
+      <TodoList todoEntries={this.todoEntries}></TodoList>
+    </div>);
+  };
 }
 
 export default App;
---
+++ src/TodoList/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TodoItem from '../TodoItem/TodoItem.js';
+
+class TodoList extends React.Component {
+    render() {
+        this.todoListElement = this.props.todoEntries.map(
+            (toDo) => (<TodoItem label={toDo.label} status={toDo.status}></TodoItem>)
+        );
+        return (<ul>
+            {this.todoListElement}
+          </ul>);
+    }
+}
+
+export default TodoList;
