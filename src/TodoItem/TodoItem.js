d9c75cf06234037be962e94b33e8313c9e9fc512
Steni Koilraj <stenikoil96@gmail.com>
2020-10-13 12:53:07 +0400

    Edit button functionality

--- src/TodoItem/TodoItem.js
+++ src/TodoItem/TodoItem.js
@@ -2,7 +2,12 @@ import React, {Component} from 'react';
 import './TodoItem.css';
 
 class TodoItem extends Component{
-    
+
+    constructor(props){
+        super(props);
+        this.state = {'label' : this.props.label, 'isReadOnly': true};
+    }
+
     onDeleteHandler = (event) => {
         this.props.onDeleteTodo(this.props.id);
     }
@@ -11,12 +16,27 @@ class TodoItem extends Component{
         this.props.onStatusChangeHandler(this.props.id, event.target.value);
     }
 
+    onEditHandler = (event) => {
+        this.setState({'isReadOnly' : false})
+    }
+    
+    onChange = (event) => {
+       this.setState({'label' : event.target.value});
+    }
+
+    onKeyDownHandler = (event) => {
+        if (event.key === "Enter") {
+            this.setState({'isReadOnly' : true});
+        }
+    }
+
     render() {
         return (
             <li>
-                {this.props.label}
+                <input className="todo" type="text" readOnly={this.state.isReadOnly} 
+                    value={this.state.label} onChange={this.onChange} onKeyDown={this.onKeyDownHandler}/>
                 <button onClick={this.onDeleteHandler}>Delete</button>
-                <button>Edit</button>
+                <button onClick={this.onEditHandler}>Edit</button>
                 <select value={this.props.status} onChange={this.onStatusChangeHandler}>
                     <option>to-do</option>
                     <option>in-progress</option>
